9c57146b6ccc049db6715ff611e5397f359de7d3
suhas <suhaskandkur2@gmail.com>
2022-07-08 12:50:17 +0530

    themes

--- components/layout/themes.js
+++ components/layout/themes.js
@@ -1,7 +1,7 @@
 const light = {
     color: "#000",
     bgColor: '#efe7fd',
-    bgImage: 'linear-gradient(180deg, #efe7fd 0%, #bdccf7 60%)',
+    bgImage: 'linear-gradient(180deg, #efe7fd 0%, #dabc40 60%)',
     bgDiv: '#fff',
     bgSubDiv: '#efe7fd'
 }
